7b08b252ca589b41364fd0ad1b05e3b61c01f347
Greg T. Wallace <greg@gregtwallace.com>
2022-04-29 17:42:59 -0400

    api hook: minor cleanup and time handling

--- src/hooks/useApiRequest.js
+++ src/hooks/useApiRequest.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 // Hook to query the API and return a state based on the query
 //    [expectedJsonName] is the json data from the expectedJsonName, named as expected
@@ -9,7 +9,7 @@ import { useEffect, useState } from "react";
 //    isLoaded is a bool that indicates if the api fetch has completed (either successfully or failed)
 const useApiRequest = (apiNode, expectedJsonName) => {
   // make a variable for original api fetch for things like 'reset' function
-  const origJsonDataName = "orig_" + expectedJsonName;
+  const origJsonDataName = 'orig_' + expectedJsonName;
 
   const [state, setState] = useState({
     [expectedJsonName]: [],
@@ -18,6 +18,19 @@ const useApiRequest = (apiNode, expectedJsonName) => {
     isLoaded: false,
   });
 
+  // function to convert unix time to something friendlier
+  const convertUnixTime = (unixTime) => {
+    return new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    }).format(unixTime * 1000);
+    // Note: *1000 due to millisecond conversion
+  };
+
   useEffect(() => {
     setState({
       [expectedJsonName]: [],
@@ -25,15 +38,15 @@ const useApiRequest = (apiNode, expectedJsonName) => {
       errorMessage: null,
       isLoaded: false,
     });
-    
+
     fetch(`${process.env.REACT_APP_API_NODE}/api${apiNode}`)
       .then((response) => response.json())
       .then((json) => {
-        if (!json[expectedJsonName]) {
+        if (!json[expectedJsonName] || json.error) {
           // add unknown error if the API didn't return the expected error json
           if (!json.error) {
-            json.error = {message: "unknown error"}
-          };
+            json.error = { message: 'unknown error' };
+          }
           // set error state
           setState({
             [expectedJsonName]: [],
@@ -41,8 +54,16 @@ const useApiRequest = (apiNode, expectedJsonName) => {
             errorMessage: json.error.message,
             isLoaded: true,
           });
-        // things worked, return json data from our expected name
+          // things worked, return json data from our expected name
         } else {
+          // If there are created or update timestamps, convert them from Unix to something friendly.
+          if (json[expectedJsonName].created_at) {
+            json[expectedJsonName].created_at = convertUnixTime(json[expectedJsonName].created_at);
+          };
+          if (json[expectedJsonName].updated_at) {
+            json[expectedJsonName].updated_at = convertUnixTime(json[expectedJsonName].updated_at);
+          };
+
           setState({
             [expectedJsonName]: json[expectedJsonName],
             [origJsonDataName]: json[expectedJsonName],
@@ -55,13 +76,13 @@ const useApiRequest = (apiNode, expectedJsonName) => {
         setState({
           [expectedJsonName]: [],
           [origJsonDataName]: [],
-          errorMessage: error.name + " " + error.message,
+          errorMessage: error.name + ' ' + error.message,
           isLoaded: true,
         });
       });
   }, [apiNode, expectedJsonName, origJsonDataName]);
 
-  return [ state, setState ];
+  return [state, setState];
 };
 
 export default useApiRequest;
