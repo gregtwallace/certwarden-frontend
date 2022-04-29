58c13988ab8c70667edfa76796cdfbb15cc28dfd
Greg T. Wallace <greg@gregtwallace.com>
2022-04-29 17:43:04 -0400

    Switch to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard';
 import AllACMEAccounts from './components/ACMEAccounts/AllACMEAccounts';
