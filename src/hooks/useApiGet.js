83f4291660ee7a397875562d664480adfa9105b5
Greg T. Wallace <greg@gregtwallace.com>
2022-05-04 17:56:11 -0400

    useApiGet: Cleanup logic / readability

--- src/hooks/useApiGet.js
+++ src/hooks/useApiGet.js
@@ -1,88 +1,90 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+// function to convert unix time to something friendlier
+const convertUnixTime = (unixTime) => {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }).format(unixTime * 1000);
+  // Note: *1000 due to millisecond conversion
+};
+
 // Hook to query the API and return a state based on the query
 const useApiGet = (apiNode, expectedJsonName) => {
   const { id } = useParams();
 
   const [state, setState] = useState({
     [expectedJsonName]: {},
-    errorMessage: null,
     isLoaded: false,
+    errorMessage: null,
   });
 
-  // function to convert unix time to something friendlier
-  const convertUnixTime = (unixTime) => {
-    return new Intl.DateTimeFormat('en-US', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    }).format(unixTime * 1000);
-    // Note: *1000 due to millisecond conversion
-  };
-
   useEffect(() => {
-    setState({
-      [expectedJsonName]: {},
-      errorMessage: null,
-      isLoaded: false,
-    });
-
-    if (parseInt(id) >= 0 || !id) {
-      // if there is an id, verify it is a valid integer or don't bother calling the API
-      fetch(`${process.env.REACT_APP_API_NODE}/api${apiNode}`)
-        .then((response) => response.json())
-        .then((json) => {
-          if (!json[expectedJsonName] || json.error) {
-            // add unknown error if the API didn't return the expected error json
-            if (!json.error) {
-              json.error = { message: 'unexpected json without error message' };
-            }
-            // set error state
-            setState({
-              [expectedJsonName]: {},
-              errorMessage: json.error.message,
-              isLoaded: true,
-            });
-          }
-          // things worked, return json data from our expected name
-          else {
-            // If there are created or update timestamps, convert them from Unix to something friendly.
-            if (json[expectedJsonName].created_at) {
-              json[expectedJsonName].created_at = convertUnixTime(
-                json[expectedJsonName].created_at
-              );
-            }
-            if (json[expectedJsonName].updated_at) {
-              json[expectedJsonName].updated_at = convertUnixTime(
-                json[expectedJsonName].updated_at
-              );
-            }
-
-            setState({
-              [expectedJsonName]: json[expectedJsonName],
-              errorMessage: null,
-              isLoaded: true,
-            });
-          }
-        })
-        .catch((error) => {
-          setState({
-            [expectedJsonName]: {},
-            errorMessage: error.name + ' ' + error.message,
-            isLoaded: true,
-          });
-        });
-    } else {
+    const fetchApi = async () => {
       setState({
         [expectedJsonName]: {},
-        errorMessage: 'id is not valid',
-        isLoaded: true,
+        isLoaded: false,
+        errorMessage: null,
       });
-    }
+
+      // if there is an id, verify it is a valid integer or don't bother calling the API
+      if (parseInt(id) >= 0 || !id) {
+        try {
+          let response = await fetch(
+            `${process.env.REACT_APP_API_NODE}/api${apiNode}`
+          );
+          let responseJson = await response.json();
+
+          if (
+            response.ok !== true ||
+            response.status !== 200 ||
+            !responseJson[expectedJsonName] ||
+            responseJson.error !== undefined
+          ) {
+            throw new Error(
+              `Status: ${response.status}, Message: ${responseJson.error.message}`
+            );
+          }
+
+          // If there are created or update timestamps, convert them from Unix to something friendly.
+          if (responseJson[expectedJsonName].created_at) {
+            responseJson[expectedJsonName].created_at = convertUnixTime(
+              responseJson[expectedJsonName].created_at
+            );
+          }
+          if (responseJson[expectedJsonName].updated_at) {
+            responseJson[expectedJsonName].updated_at = convertUnixTime(
+              responseJson[expectedJsonName].updated_at
+            );
+          }
+
+          setState({
+            [expectedJsonName]: responseJson[expectedJsonName],
+            isLoaded: true,
+            errorMessage: null,
+          });
+        } catch (error) {
+          setState({
+            [expectedJsonName]: {},
+            isLoaded: true,
+            errorMessage: error.toString(),
+          });
+        }
+      } else {
+        setState({
+          [expectedJsonName]: {},
+          isLoaded: true,
+          errorMessage: 'Message: id is not valid',
+        });
+      }
+    };
+
+    fetchApi();
   }, [apiNode, expectedJsonName, id]);
 
   return state;
