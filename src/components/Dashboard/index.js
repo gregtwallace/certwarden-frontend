7882515eb023bf7b7ef72a4dd1a3005d4deb41b9
Greg T. Wallace <greg@gregtwallace.com>
2022-08-27 14:13:21 -0400

    dashboard: update formatting

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -13,9 +13,39 @@ import TableHeader from '../UI/Table/TableHeader';
 import TableRow from '../UI/Table/TableRow';
 import H2Header from '../UI/Header/H2Header';
 
+// Component for expiration times / formatting
+const Expiration = (props) => {
+  // do some formatting if certs are nearing EoL
+  var daysToExp = daysUntil(props.valid_to);
+  var classes = 'font-weight-bold';
+
+  switch (true) {
+    // 30 days left
+    case daysToExp <= 30:
+      classes += ' bg-danger text-white';
+      break;
+
+    // 40 days left
+    case daysToExp <= 40:
+      classes += ' text-danger';
+      break;
+
+    default:
+      classes += ' text-info';
+      break;
+  }
+
+  return (
+    <>
+      {convertUnixTime(props.valid_to) + ' '}
+      <span className={classes}>{'(' + daysToExp + ' Days)'}</span>
+    </>
+  );
+};
+
+// Main Dashboard
 const Dashboard = () => {
   const [apiGetState] = useAxiosGet('/v1/orders/currentvalid', 'orders', true);
-
 
   if (apiGetState.errorMessage) {
     return <ApiError>{apiGetState.errorMessage}</ApiError>;
@@ -24,8 +54,7 @@ const Dashboard = () => {
   } else {
     return (
       <>
-        <H2Header h2='Dashboard'>
-        </H2Header>
+        <H2Header h2='Dashboard'></H2Header>
         <Table>
           <TableHead>
             <TableRow>
@@ -38,16 +67,22 @@ const Dashboard = () => {
             {apiGetState.orders &&
               apiGetState.orders.map((m) => (
                 <TableRow key={m.id}>
-                  <TableHeader>{m.certificate.subject} {m.certificate.acme_account.is_staging && <span className="text-info">(Staging)</span>}</TableHeader>
+                  <TableHeader>
+                    {m.certificate.subject}{' '}
+                    {m.certificate.acme_account.is_staging && (
+                      <span className='text-info'>(Staging)</span>
+                    )}
+                  </TableHeader>
                   <TableData scope='row'>
-                    <Link to={'/certificates/' + m.certificate.id}>{m.certificate.name}</Link>
+                    <Link to={'/certificates/' + m.certificate.id}>
+                      {m.certificate.name}
+                    </Link>
                   </TableData>
                   <TableData>
-                    {convertUnixTime(m.valid_to) + " (" + daysUntil(m.valid_to) + " Days)"} 
+                    <Expiration valid_to={m.valid_to} />
                   </TableData>
                 </TableRow>
-              ))
-              }
+              ))}
           </TableBody>
         </Table>
       </>
