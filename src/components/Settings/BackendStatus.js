2ae28f0a16fba22ecfe3c18d3b948017b1a52514
Greg T. Wallace <greg@gregtwallace.com>
2022-11-05 17:18:46 -0400

    settings: updates to status

--- src/components/Settings/BackendStatus.js
+++ src/components/Settings/BackendStatus.js
@@ -13,15 +13,27 @@ const BackendStatus = () => {
     return <ApiLoading />;
   } else {
     return (
-      <div className="container mb-5">
-        <H5Header h5="Backend Status" />
+      <div className='container mb-5'>
+        <H5Header h5='Backend Status' />
 
         <p>Status: {apiGetState.server.status}</p>
-        <p>Development Mode: {apiGetState.server.development_mode ? "true" : "false"}</p>
+        <p>
+          Development Mode:{' '}
+          {apiGetState.server.development_mode ? 'true' : 'false'}
+        </p>
         <p>Backend Version: {apiGetState.server.version}</p>
-        <p>ACME Production Directory: {apiGetState.server.acme_directories.prod}</p>
-        <p>ACME Staging Directory: {apiGetState.server.acme_directories.staging}</p>
-      
+        <p>Backend API URL: {apiGetState.server.api_url}</p>
+
+        {apiGetState.server.frontend_url && (
+          <p>Frontend URL: {apiGetState.server.frontend_url}</p>
+        )}
+
+        <p>
+          ACME Production Directory: {apiGetState.server.acme_directories.prod}
+        </p>
+        <p>
+          ACME Staging Directory: {apiGetState.server.acme_directories.staging}
+        </p>
       </div>
     );
   }
