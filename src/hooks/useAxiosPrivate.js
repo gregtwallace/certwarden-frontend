6118258c11f36ebac18412430472b0431a8ce1d2
Greg T. Wallace <greg@gregtwallace.com>
2022-12-23 00:27:54 -0500

    axios private: prevent multiple refresh attempts

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -1,10 +1,14 @@
 import { useEffect } from 'react';
 
 import { axiosPrivate } from '../api/axios';
-
 import useRefreshToken from './useRefreshToken';
 
+// name of anti-retry header
 const NO_RETRY_HEADER = 'x-no-retry';
+
+// var to monitor of an api call is already trying to refresh the
+// access_token. Used to skip refresh attempt if another call tried it.
+var isRefreshing = false;
 
 const useAxiosPrivate = () => {
   const refresh = useRefreshToken();
@@ -31,13 +35,29 @@ const useAxiosPrivate = () => {
       (response) => response,
       async (error) => {
         var prevRequest = error?.config;
+        // if the error was 401 & not already a retry, try refresh
         if (
           error?.response?.status === 401 &&
           prevRequest?.headers[NO_RETRY_HEADER] == null
         ) {
-          // get new token, return error if refresh errors
+          // if refresh is already taking place, don't call refresh multiple times, just wait
+          // and then retry original request again
+          if (isRefreshing) {
+            while (isRefreshing) {
+              // sleep before checking isRefreshing again
+              await new Promise((resolve) => setTimeout(resolve, 100));
+            }
+            // try new access token, don't allow retry
+            prevRequest.headers[NO_RETRY_HEADER] = 'true';
+            prevRequest.headers['Authorization'] =
+              sessionStorage.getItem('access_token');
+            return axiosPrivate(prevRequest);
+          }
+          // no already refreshing - get new token, return error if refresh errors
           try {
+            isRefreshing = true;
             const newAccessToken = await refresh();
+            isRefreshing = false;
 
             // retry with new token
             prevRequest.headers[NO_RETRY_HEADER] = 'true';
