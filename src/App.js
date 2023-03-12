118b61a0711a6015bfcba2062e97102f0c3b9d6c
Greg T. Wallace <greg@gregtwallace.com>
2023-03-12 13:26:00 -0400

    v0.6.11

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Main from './components/Main/Main';
 import Footer from './Footer';
 
 // react app version
-export const frontendVersion = "0.6.10";
+export const frontendVersion = "0.6.11";
 
 // value for new records on backend
 export const newId = -1;
