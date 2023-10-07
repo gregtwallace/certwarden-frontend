d2af77b88c3d639f7fe1bd433a429ccc7f79dd06
Greg T. Wallace <greg@gregtwallace.com>
2023-10-07 14:05:07 -0400

    griditemcontainer: support sx

--- src/components/UI/Grid/GridItemContainer.jsx
+++ src/components/UI/Grid/GridItemContainer.jsx
@@ -2,8 +2,14 @@ import PropTypes from 'prop-types';
 import { Container, Paper } from '@mui/material';
 
 const GridItemContainer = (props) => {
+  const sx = {
+    ...props.sx,
+    p: 1,
+    height: 1,
+  };
+
   return (
-    <Container component={Paper} sx={{ p: 1, height: 1 }}>
+    <Container component={Paper} sx={sx}>
       {props.children}
     </Container>
   );
@@ -11,6 +17,7 @@ const GridItemContainer = (props) => {
 
 GridItemContainer.propTypes = {
   children: PropTypes.node,
-}
+  sx: PropTypes.object,
+};
 
 export default GridItemContainer;
