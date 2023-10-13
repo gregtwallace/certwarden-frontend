11c316f85bfe561d5774215a1a331e99279e620d
Greg T. Wallace <greg@gregtwallace.com>
2023-10-12 22:40:36 -0400

    provider: config view remove dep func

--- src/components/Routes/Providers/ViewOneProvider/ConfigView.jsx
+++ src/components/Routes/Providers/ViewOneProvider/ConfigView.jsx
@@ -9,7 +9,7 @@ const keyPretty = (key) => {
   const words = key.split('_');
 
   for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    words[i] = words[i][0].toUpperCase() + words[i].substring(1);
   }
 
   return words.join(' ');
