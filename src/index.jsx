ca2adf5d7f2cf6bcb03a7133fc03f5fd1f0a7bf0
Greg T. Wallace <greg@gregtwallace.com>
2023-10-21 11:02:09 -0400

    add csp support * remove data: roboto font source - workaround for Vite csp limitation * inject csp placeholders on index.html elems * cannot use style nonce (yet) - not yet supported by Vite

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AuthExpiresProvider } from './context/AuthExpiresProvider';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // add preload attribute and nonce place holder
+    {
+      name: 'html-inject-data',
+      enforce: 'post',
+      transformIndexHtml(html) {
+        const regex = /<(style|script|link) (.*)>/gi;
+        const replacer = (_, p1, p2) => {
+          // add nonce?
+          if (
+            p1 === 'style' ||
+            p1 === 'script' ||
+            // if link, only nonce for stylesheet
+            (p1 === 'link' && p2.includes('rel="stylesheet"'))
+          ) {
+            p2 = `nonce="{SERVER-CSP-NONCE}" ${p2}`;
+          }
+
+          // always add data preload
+          return `<${p1} data-preload="true" ${p2}>`;
+        };
+
+        return html.replace(regex, replacer);
+      },
+    },
+  ],
   base: '/legocerthub/app',
-})
+});
