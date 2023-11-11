f1ba7f91dab17e0ec1dcace9a93838a301d245cf
Greg T. Wallace <greg@gregtwallace.com>
2023-11-11 10:25:50 -0500

    ts: type safe input change handler

--- src/helpers/input-handler.ts
+++ src/helpers/input-handler.ts
@@ -1,15 +1,58 @@
 import type { ChangeEvent, Dispatch, SetStateAction } from 'react';
 import { type SelectChangeEvent } from '@mui/material';
 
+import { z } from 'zod';
+
 // import { redactJSONObject } from "./logging";
 
-type stateValueTypes = string | number | object | boolean | undefined;
 type valueConversionTypes = 'unchanged' | 'number' | false | true;
 
-const setObjPathVal = <T extends object, K>(
+// modify an object in place using its path
+// WARNING: IF CHANGE TYPES, MANUALLY UPDATE ZOD consts too!
+type settableValuesType =
+  | string
+  | number
+  | boolean
+  | settableObjectType
+  | settableArrayType
+  | undefined;
+// WARNING SEE ABOVE
+type settableObjectType = { [key: string]: settableValuesType };
+type settableArrayType = Array<settableValuesType>;
+
+let settableObject: z.ZodType<settableObjectType>;
+let settableArray: z.ZodType<settableArrayType>;
+
+// WARNING: SEE ABOVE - must stay in sync with above types
+const settableValues: z.ZodType<settableValuesType> = z.union([
+  z.string(),
+  z.number(),
+  z.boolean(),
+  z.lazy(() => settableObject),
+  z.lazy(() => settableArray),
+  z.undefined(),
+]);
+settableObject = z.record(
+  z.string(),
+  z.lazy(() => settableValues)
+);
+settableArray = z.array(z.lazy(() => settableValues));
+
+const isSettableObjectType = (unk: unknown): unk is settableObjectType => {
+  const { success } = settableObject.safeParse(unk);
+  return success;
+};
+const isSettableArrayType = (unk: unknown): unk is settableArrayType => {
+  const { success } = settableArray.safeParse(unk);
+  return success;
+};
+
+// const settableValues: z.ZodType<settableValuesType> = z.lazy()
+
+const setObjPathVal = <T extends settableObjectType | settableArrayType>(
   obj: T,
   path: string,
-  value: K
+  value: settableValuesType
 ): T => {
   // missing args
   if (!obj) return {} as T;
@@ -19,33 +62,76 @@ const setObjPathVal = <T extends object, K>(
   const segments = path.split(/[.[\]]/g).filter((x) => !!x.trim());
 
   // setter
-  const _set = (node: any): void => {
+  const _set = (node: settableValuesType): void => {
+    // more nesting to do
+
     if (segments.length > 1) {
-      const key = segments.shift() as keyof typeof node;
+      const key = segments.shift();
+      if (!key) {
+        throw '(should be) impossible error happened in object setter (key undefined)';
+      }
 
-      const nextIsNum = !isNaN(parseInt(segments[0] || 'not int'))
-        ? false
-        : true;
+      const nextSegment = segments[0];
+      if (!nextSegment) {
+        throw '(should be) impossible error happened in object setter (next segment undefined)';
+      }
 
-      if (node[key] !== undefined) {
-        // no-op
-      } else if (nextIsNum) {
-        node[key] = [];
+      const nextIsNum = !isNaN(parseInt(nextSegment)) ? true : false;
+
+      // set next node based on next segment type (string or number)
+      if (isSettableObjectType(node)) {
+        // make new node if doesn't exist
+        // or make new if it isn't the right kind
+        if (
+          !node[key] ||
+          (nextIsNum && !isSettableArrayType(node[key])) ||
+          (!nextIsNum && !isSettableObjectType(node[key]))
+        ) {
+          node[key] = nextIsNum
+            ? <settableArrayType>[]
+            : <settableObjectType>{};
+        }
+
+        _set(node[key]);
+      } else if (isSettableArrayType(node)) {
+        // make new node if doesn't exist
+        // or make new if it isn't the right kind
+        const keyInt = parseInt(key);
+        if (
+          !node[keyInt] ||
+          (nextIsNum && !isSettableArrayType(node[keyInt])) ||
+          (!nextIsNum && !isSettableObjectType(node[keyInt]))
+        ) {
+          node[keyInt] = nextIsNum
+            ? <settableArrayType>[]
+            : <settableObjectType>{};
+        }
+
+        _set(node[keyInt]);
       } else {
-        node[key] = {};
+        throw '(should be) impossible error happened in object setter (node is not object or array)';
+      }
+    } else {
+      // set final val
+      const finalKey = segments[0];
+      if (!finalKey) {
+        throw '(should be) impossible error happened in object setter (final key undefined)';
       }
 
-      _set(node[key]);
-    } else {
-      const finalKey = segments[0] as keyof typeof node;
-      if (!finalKey) {
-        throw '(should be) impossible error happened in object setter';
+      // obj or array?
+      if (isSettableObjectType(node)) {
+        node[finalKey] = value;
+      } else if (isSettableArrayType(node)) {
+        const finalKeyInt = parseInt(finalKey);
+        node[finalKeyInt] = value;
+      } else {
+        throw '(should be) impossible error happened in object setter (final node is not object or array)';
       }
-      node[finalKey] = value;
     }
   };
 
-  const newObj = {...obj}
+  // can't directly modify prev state, so just grab all of the props
+  const newObj = { ...obj };
   _set(newObj);
 
   return newObj;
@@ -59,7 +145,7 @@ export type inputOption = {
   name: string;
   alsoSet?: {
     name: string;
-    value: stateValueTypes;
+    value: settableValuesType;
   }[];
 };
 
@@ -80,9 +166,7 @@ export type inputHandlerFunc = (
 
 // formChangeHandlerFunc returns the input change handler specific
 // to the setFormState func that is passed in
-export const inputHandlerFuncMaker = <
-  StateObject extends Record<string, unknown>
->(
+export const inputHandlerFuncMaker = <StateObject extends settableObjectType>(
   setFormState: Dispatch<SetStateAction<StateObject>>
 ): inputHandlerFunc => {
   return (
@@ -94,7 +178,7 @@ export const inputHandlerFuncMaker = <
   ) => {
     // set newVal to the real value to store in state
     const inputValue = event.target.value;
-    let value: stateValueTypes = inputValue;
+    let value: settableValuesType = inputValue;
 
     // attempt to convert value if needed
     if (convertValueTo === 'number') {
