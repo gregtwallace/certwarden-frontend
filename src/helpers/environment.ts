af42667dd81188d5674f751d0ae4de995c3cf74e
Greg T. Wallace <greg@gregtwallace.com>
2024-08-13 22:33:08 -0400

    lint environment.ts

--- src/helpers/environment.ts
+++ src/helpers/environment.ts
@@ -1,3 +1,13 @@
+declare global {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface Window {
+    env: {
+      API_URL: string;
+      SHOW_DEBUG_INFO: string;
+    };
+  }
+}
+
 // environment file variables
 // use process.env in dev and window.env (env.js) in prod
 const useFile = import.meta.env.PROD === true;
@@ -5,7 +15,7 @@ const useFile = import.meta.env.PROD === true;
 // api url  API_URL: '__API_URL__',
 export const apiUrl = useFile
   ? window.env.API_URL
-  : import.meta.env.VITE_API_URL;
+  : import.meta.env['VITE_API_URL'];
 
 // debug logging - SHOW_DEBUG_INFO: '__SHOW_DEBUG_INFO__',
 // Note: This is SAFE to run in production, it just provides extra info to users
@@ -14,4 +24,4 @@ export const apiUrl = useFile
 // - provides some additional information on various pages (such as IDs)
 export const showDebugInfo = useFile
   ? window.env.SHOW_DEBUG_INFO
-  : import.meta.env.VITE_SHOW_DEBUG_INFO;
+  : import.meta.env['VITE_SHOW_DEBUG_INFO'];
