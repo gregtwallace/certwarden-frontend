f717b7cdfb18a62e013c0f64e2132de257e313c6
Greg T. Wallace <greg@gregtwallace.com>
2025-02-08 12:52:40 -0500

    linting updates and a couple minor fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+    ],
+
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -23,6 +32,28 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'no-unused-vars': 'off',
+
+      // TODO: Fix / re-enable these
+      '@typescript-eslint/no-unused-vars': 'off',
+      // '@typescript-eslint/no-unused-vars': [
+      //   'error',
+      //   {
+      //     vars: 'all',
+      //     args: 'after-used',
+      //     caughtErrors: 'all',
+      //     ignoreRestSiblings: false,
+      //     reportUsedIgnorePattern: true,
+      //     varsIgnorePattern: '^_',
+      //     argsIgnorePattern: '^_',
+      //     caughtErrorsIgnorePattern: '^_',
+      //   },
+      // ],
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      '@typescript-eslint/dot-notation': 'off',
+      '@typescript-eslint/prefer-regexp-exec': 'off',
     },
-  },
-)
+  }
+);
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
