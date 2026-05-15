952d8f976f1f30f2c28793da8dbbae19f412e630
Greg T. Wallace <greg@gregtwallace.com>
2026-05-15 17:37:00 -0400

    logging redaction: improve conditions
    * Don't redact non-strings (e.g., fixes unnecessary redaction of `api_key_disabled`
    * Don't redact 0 length strings
    * Special case for arrays so the array length is maintained but each string element is redacted
    * Redact `post_processing_client_key`
    * Remove type cast
    * Return undefined as undefined instead of an empty object

--- src/helpers/logging.ts
+++ src/helpers/logging.ts
@@ -7,28 +7,54 @@ const redactObjectKeysContaining = [
   'hmac_key',
   'pem',
   'environment',
+  'post_processing_client_key',
 ];
 
 // redactJSONObject redacts any keys in the object containing strings
 // in the list of keys to redact. this is intended to stop console log
 // of things like passwords
-export const redactJSONObject = (object: object | undefined): object => {
-  // if undefined, return empty object
+export const redactJSONObject = (
+  object: object | undefined,
+): object | undefined => {
+  // if undefined, return undefined
   if (!object) {
-    return {};
+    return undefined;
   }
 
   // redact any keys containing words in the redaction array
   // easiest way is to stringify and then parse
   const redactedJSONString = JSON.stringify(object, (key, val: unknown) => {
-    // search for any of the strings in the array
-    const redactVal = redactObjectKeysContaining.some((str) =>
-      key.includes(str)
-    );
+    // should we even consider redaction? if key doesn't contain one of the values
+    // we're concerned about, don't redact
+    const keyIsInRedactList = redactObjectKeysContaining.some((innerVal) => {
+      return key.includes(innerVal);
+    });
+    if (!keyIsInRedactList) {
+      return val;
+    }
 
-    return redactVal ? '[redacted for log]' : val;
+    // its a non-empty string, redact it
+    if (typeof val === 'string' && val.length > 0) {
+      return '[redacted for log]';
+    }
+
+    // its an array, redact non-empty string elements
+    if (Array.isArray(val)) {
+      return Array.from(val, (elem) =>
+        typeof elem === 'string' && elem.length > 0
+          ? '[redacted for log]'
+          : (elem as unknown),
+      );
+    }
+
+    return val;
   });
 
   // turn back into object
-  return JSON.parse(redactedJSONString) as object;
+  const obj: unknown = JSON.parse(redactedJSONString);
+  if (obj === null || typeof obj !== 'object') {
+    throw new Error('json parse failed unexpectedly');
+  }
+
+  return obj;
 };
